feb0e359098743093382af52a31aa3a1da08003f
lehuuhuy1511 <146902635+lehuuhuy1511@users.noreply.github.com>
2024-10-21 17:17:06 +0700

    21/10

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const express = require ('express')
+const qpp = express()
+const port = 8080
+
+app.get('/',(req, res) => res.send ('Helo world'))
+
+app.listen(port, ()=> console.log(` Example app listening at http://localhost:${port}`))
+const someModule = require('some-module');
+
 //var http = require('http');
 
 //http.createServer(function (req, res) {
@@ -62,58 +71,25 @@
     //return res.end();
   //}
 //}).listen(8080);
-//var nodemailer = require ('nodemailer');
-//var transporter = nodemailer.createTransport({
-  //  service :'gmail',
-  //  auth:{
-    //    user: ' youremail@gmail.com',
-    //    pass: 'yoyrpassword'
-    //}
-//})
-  
+var http = require('http');
+var formidable = require('formidable');
 
-var mysql = require('mysql')
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      res.write('File uploaded');
+      res.end();
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
 
-var con = mysql.createConnection({
-  host: "localhoost",
-  user:"yourusername",
-  password: "yourqassword",
-});
+const myLocalModule = require('./path/to/myLocalModule');
 
-con.connect(function(err){
-  if (err) throw err;
-  console.log("conneected!");
-  con.query ("CREATE DATABASE mysql", function (err,result){
-    if (err) throw err ; 
-    console.log (" Database created"); 
-  })
-});
-
-
-
-var mysql = require('mýql');
-var con = mysql.createCommection({
-  host: "localhost",
-  user :"yourusername",
-  password:" yourpassword"
-});
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-});
--- mysql.js
+++ mysql.js
@@ -5,8 +5,214 @@ var con = mysql.createConnection({
   user: "yourusername",
   password: "yourpassword"
 });
+exports.con = con;
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+exports.con = con;
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+exports.con = con;
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+exports.con = con;
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+exports.con = con;
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+exports.con = con;
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+exports.con = con;
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers ORDER BY name", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+var mysql = require('mysql');
+
+var con = mysql. createConnection({
+  host: "localhost",
+  user : "yourusername",
+  password :"yourpassword",
+  database:"mydb"
+});
+exports.con = con;
+
+con.connect(function(err){
+  if (err) throw err;
+  var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+  con.query (sql, function (er, result ){
+    if (err) throw err;
+    console.log ("Nunmber of records deleted: " + result.afectedRows);
+  });
+});
+
+
+var mysql = require ('mysql');
+
+var con = mysql.createConnection({
+  host :" localhost",
+  user :"yourusername",
+  password:"yourpassword",
+  database : "mydb"
+});
+exports.con = con;
+
+con.connect (function(err){
+  if(err)throw err
+  var sql = " DROP TABLE customers";
+  con.query (sql , function( err , result ){
+    if (err) throw err;
+    console.log ("table deleted");
+  });
+});
+
+var mysql = require ('mysql')
+
+var con = mysql.createConnection({
+  host : "localhost",
+  user : " yourusername",
+  password:"yourpassword",
+  database:"mydb"
+});
+exports.con = con;
+con.connect(function(err){
+  if (err) throw err
+  var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
+});
+
+var mysql = require ('mysql')
+
+var con = mysql.createConnection({
+  host:'localhoot',
+  user: 'yourusername',
+  password:'yourpassword',
+  database :'mydb'
+});
+exports.con = con;
+
+con.connect(function(err){
+  if (err) throw err ;
+  var sql = "SEELECT * FROM customers LIMIT5  5";
+  con.query(sql , function (err , result){
+    if (err) throw err;
+    console.log ( sesult);
+  });
+});
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+exports.con = con;
+
